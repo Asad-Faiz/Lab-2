e9e3f43c5082a67758264d68b254287b9b13dc12
asad faiz <MuhammadAsadFaiz2000@gmail.com>
2022-03-27 19:31:38 +0500

    Lab2

---
+++ lib/task2.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("I Am Rick"),
+        ),
+        backgroundColor: Colors.lightGreenAccent,
+        body: Center(
+            child: Container(
+                width: double.infinity,
+                child: Image.asset(
+                  "images/rick.PNG",
+                  fit: BoxFit.fill,
+                ))),
+      ),
+    );
+  }
+}
